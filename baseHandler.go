90e55e1c5ba1f59802cd3cb064c749a4082891d5
Geoff Ford <gford1000@hotmail.com>
2021-11-25 21:01:53 +0000

    Introduced differentiated logging

--- baseHandler.go
+++ baseHandler.go
@@ -7,28 +7,40 @@ import (
 
 // baseHandler drives the handling of requests
 type baseHandler struct {
-	config *cacheConfig
-	handler func(w http.ResponseWriter, req *http.Request)
-	logger *logger
-	method string
-	pattern string
+	config    *cacheConfig
+	handler   func(w http.ResponseWriter, req *http.Request)
+	logger    *logger
+	method    string
+	pattern   string
 	requestID string
 }
 
 // Log ensures that the requestID is always applied to the logs
-func (b *baseHandler) Log(msg string) {
-	b.logger.Println(fmt.Sprintf("%v %v", b.requestID, msg))
+func (b *baseHandler) Info(msg string) {
+	b.logger.Println(fmt.Sprintf("%v INFO  %v", b.requestID, msg))
+}
+
+func (b *baseHandler) Debug(msg string) {
+	b.logger.Println(fmt.Sprintf("%v DEBUG %v", b.requestID, msg))
+}
+
+func (b *baseHandler) Error(msg string) {
+	b.logger.Println(fmt.Sprintf("%v ERROR %v", b.requestID, msg))
+}
+
+func (b *baseHandler) Warn(msg string) {
+	b.logger.Println(fmt.Sprintf("%v WARN  %v", b.requestID, msg))
 }
 
 // Process generates the response for a request
 func (b *baseHandler) Process(w http.ResponseWriter, req *http.Request) {
 
-	b.Log(fmt.Sprintf("Processing %v", b.pattern))
-	defer b.Log(fmt.Sprintf("Completed %v", b.pattern))
+	b.Info(fmt.Sprintf("Processing %v", b.pattern))
+	defer b.Info(fmt.Sprintf("Completed %v", b.pattern))
 
 	defer func() {
 		if r := recover(); r != nil {
-			b.Log(fmt.Sprintf("Processing error %v", r))
+			b.Error(fmt.Sprintf("Processing error %v", r))
 			returnError(w, "Request error", http.StatusBadRequest)
 		}
 	}()
--- cache.go
+++ cache.go
@@ -40,48 +40,49 @@ func (b *baseHandler) getCacheFileName(info *pageInfo) string {
 
 // zipData applies default gzip to the supplied byte slice
 func (b *baseHandler) zipData(data []byte, token string) ([]byte, error) {
-	b.Log(fmt.Sprintf("Page %v: GZipping", token))
+	b.Debug(fmt.Sprintf("Page %v: GZipping", token))
 
 	var buf bytes.Buffer
 	zw := gzip.NewWriter(&buf)
 
 	_, err := zw.Write(data)
 	if err != nil {
-		b.Log(fmt.Sprintf("Page %v: GZip write error - %v", token, err))
+		b.Error(fmt.Sprintf("Page %v: GZip write error - %v", token, err))
 		return nil, err
 	}
 	zw.Close()
 
+	b.Debug(fmt.Sprintf("Page %v: GZipped", token))
 	return buf.Bytes(), nil
 }
 
 // unzipData applies gzip decompression to the supplied byte slice
 func (b *baseHandler) unzipData(data []byte, token string) ([]byte, error) {
-	b.Log(fmt.Sprintf("Page %v: Ungzipping", token))
+	b.Debug(fmt.Sprintf("Page %v: Ungzipping", token))
 
 	buf := bytes.NewBuffer(data)
 
 	zr, err := gzip.NewReader(buf)
 	if err != nil {
-		b.Log(fmt.Sprintf("Page %v: Zip buffer error - %v", token, err))
+		b.Error(fmt.Sprintf("Page %v: Zip buffer error - %v", token, err))
 		return nil, fmt.Errorf("internal failure handling page (4)")
 	}
 	defer zr.Close()
 
 	data, err = ioutil.ReadAll(zr)
 	if err != nil {
-		b.Log(fmt.Sprintf("Page %v: Zip read error - %v", token, err))
+		b.Error(fmt.Sprintf("Page %v: Zip read error - %v", token, err))
 		return nil, fmt.Errorf("internal failure handling page (4)")
 	}
 
-	b.Log(fmt.Sprintf("Page %v: Ungzipped", token))
+	b.Debug(fmt.Sprintf("Page %v: Ungzipped", token))
 	return data, nil
 }
 
 // writePage creates an encrypted page from the slice
 func (b *baseHandler) writePage(data []byte, info *pageInfo) error {
-	b.Log(fmt.Sprintf("Page %v: Writing", info.token))
-	defer b.Log(fmt.Sprintf("Page %v: Completed", info.token))
+	b.Debug(fmt.Sprintf("Page %v: Writing", info.token))
+	defer b.Debug(fmt.Sprintf("Page %v: Completed", info.token))
 
 	// Apply gzip if specified
 	if b.config.zip {
@@ -92,45 +93,43 @@ func (b *baseHandler) writePage(data []byte, info *pageInfo) error {
 		}
 	}
 
-	b.Log(fmt.Sprintf("Page %v: GZipped", info.token))
-
 	// If a key is provided, assume the page is to be encrypted
 	if len(b.config.key) > 0 {
-		b.Log(fmt.Sprintf("Page %v: Encrypting", info.token))
+		b.Debug(fmt.Sprintf("Page %v: Encrypting", info.token))
 
 		c, err := aes.NewCipher(b.config.key)
 		if err != nil {
-			b.Log(fmt.Sprintf("Page %v: Error creating Cipher - %v", info.token, err))
+			b.Error(fmt.Sprintf("Page %v: Error creating Cipher - %v", info.token, err))
 			return fmt.Errorf("invalid page data")
 		}
 
 		gcm, err := cipher.NewGCM(c)
 		if err != nil {
-			b.Log(fmt.Sprintf("Page %v: Error creating GCM - %v", info.token, err))
+			b.Error(fmt.Sprintf("Page %v: Error creating GCM - %v", info.token, err))
 			return fmt.Errorf("internal failure creating page (1)")
 		}
 
 		nonce := make([]byte, gcm.NonceSize())
 		if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-			b.Log(fmt.Sprintf("Page %v: Error creating Nonce - %v", info.token, err))
+			b.Error(fmt.Sprintf("Page %v: Error creating Nonce - %v", info.token, err))
 			return fmt.Errorf("internal failure creating page (2)")
 		}
 
 		data = gcm.Seal(nonce, nonce, data, nil)
 
-		b.Log(fmt.Sprintf("Page %v: Completed encryption", info.token))
+		b.Debug(fmt.Sprintf("Page %v: Completed encryption", info.token))
 	}
 
-	b.Log(fmt.Sprintf("Page %v: Writing to Disk", info.token))
+	b.Debug(fmt.Sprintf("Page %v: Writing to Disk", info.token))
 	fileName, err := b.createCacheFileName(info)
 	if err != nil {
-		b.Log(fmt.Sprintf("Page %v: Error writing to disk - %v", info.token, err))
+		b.Error(fmt.Sprintf("Page %v: Error writing to disk - %v", info.token, err))
 		return err
 	}
 	err = ioutil.WriteFile(fileName, data, 0644)
-	b.Log(fmt.Sprintf("Page %v: Writing to Disk Completed", info.token))
+	b.Debug(fmt.Sprintf("Page %v: Writing to Disk Completed", info.token))
 	if err != nil {
-		b.Log(fmt.Sprintf("Page %v: Error writing to disk - %v", info.token, err))
+		b.Error(fmt.Sprintf("Page %v: Error writing to disk - %v", info.token, err))
 	}
 
 	return err
@@ -138,49 +137,49 @@ func (b *baseHandler) writePage(data []byte, info *pageInfo) error {
 
 // retrievePage returns decrypted byte slice
 func (b *baseHandler) retrievePage(info *pageInfo) (page []byte, err error) {
-	b.Log(fmt.Sprintf("Page %v: Retrieving", info.token))
-	defer b.Log(fmt.Sprintf("Page %v: Completed retrieval", info.token))
+	b.Info(fmt.Sprintf("Page %v: Retrieving", info.token))
+	defer b.Info(fmt.Sprintf("Page %v: Completed retrieval", info.token))
 
 	pageLocation := b.getCacheFileName(info)
 
-	b.Log(fmt.Sprintf("Page %v: Reading from Disk", info.token))
+	b.Debug(fmt.Sprintf("Page %v: Reading from Disk", info.token))
 	page, err = ioutil.ReadFile(pageLocation)
-	b.Log(fmt.Sprintf("Page %v: Reading from Disk completed", info.token))
+	b.Debug(fmt.Sprintf("Page %v: Reading from Disk completed", info.token))
 	if err != nil {
-		b.Log(fmt.Sprintf("Page %v: Error reading from disk - %v", info.token, err))
+		b.Error(fmt.Sprintf("Page %v: Error reading from disk - %v", info.token, err))
 		return nil, fmt.Errorf("invalid request or page token")
 	}
 
 	// If a key is provided, assume the page is encrypted
 	if len(b.config.key) > 0 {
-		b.Log(fmt.Sprintf("Page %v: Decrypting", info.token))
+		b.Debug(fmt.Sprintf("Page %v: Decrypting", info.token))
 
 		c, err := aes.NewCipher(b.config.key)
 		if err != nil {
-			b.Log(fmt.Sprintf("Page %v: Error creating Cipher - %v", info.token, err))
+			b.Error(fmt.Sprintf("Page %v: Error creating Cipher - %v", info.token, err))
 			return nil, fmt.Errorf("invalid page data")
 		}
 
 		gcm, err := cipher.NewGCM(c)
 		if err != nil {
-			b.Log(fmt.Sprintf("Page %v: Error creating GCM - %v", info.token, err))
+			b.Error(fmt.Sprintf("Page %v: Error creating GCM - %v", info.token, err))
 			return nil, fmt.Errorf("internal failure handling page (1)")
 		}
 
 		nonceSize := gcm.NonceSize()
 		if len(page) < nonceSize {
-			b.Log(fmt.Sprintf("Page %v: Data inconsistency", info.token))
+			b.Error(fmt.Sprintf("Page %v: Data inconsistency", info.token))
 			return nil, fmt.Errorf("internal failure handling page (2)")
 		}
 
 		nonce, data := page[:nonceSize], page[nonceSize:]
 		page, err = gcm.Open(nil, nonce, data, nil)
 		if err != nil {
-			b.Log(fmt.Sprintf("Page %v: Error decrypting - %v", info.token, err))
+			b.Error(fmt.Sprintf("Page %v: Error decrypting - %v", info.token, err))
 			return nil, fmt.Errorf("internal failure handling page (3)")
 		}
 
-		b.Log(fmt.Sprintf("Page %v: Decrypted", info.token))
+		b.Debug(fmt.Sprintf("Page %v: Decrypted", info.token))
 	}
 
 	// unzip if requested
